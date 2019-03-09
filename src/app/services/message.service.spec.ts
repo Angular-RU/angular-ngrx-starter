645d152ed5844476e5b7bb274d8f08d8f7ea2b4b
klimentru1986 <kliment1986@inbox.ru>
2019-03-09 11:17:32 +0300

    message service tests

--- src/app/services/message.service.spec.ts
+++ src/app/services/message.service.spec.ts
@@ -3,13 +3,22 @@ import { TestBed, inject } from '@angular/core/testing';
 import { MessageService } from './message.service';
 
 describe('MessageService', () => {
+  let messageService: MessageService;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [MessageService]
-    });
+    messageService = new MessageService();
   });
 
-  it('should be created', inject([MessageService], (service: MessageService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should add message', () => {
+    messageService.add('test message');
+
+    expect(messageService.messages).toMatchSnapshot();
+  });
+
+  it('should clear messages', () => {
+    messageService.add('test message');
+    messageService.clear();
+    
+    expect(messageService.messages).toMatchSnapshot();
+  });
 });
